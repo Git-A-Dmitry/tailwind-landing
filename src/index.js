d4de7ffe67e08df0c6067aa33a6fb5329c2bdbfd
Dmitry A <adcntt@gmail.com>
2023-02-02 00:43:34 +0300

    toggle menu

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// function toggleMenu(e) {
+//   let list = document.querySelector('ul');
+
+//   e.name === 'menu' ? ((e.name = 'close'), list.classList.add('top-[80px]'), list.classList.add('opacity-100')) : ((e.name = 'menu'), list.classList.remove('top-[80px]'), list.classList.remove('opacity-100'));
+// }
+
+// const toggleBtn = document.querySelector('#toggle');
+// const nav = document.querySelector('nav ul');
+
+// let navIsOpen = false;
+
+// toggleBtn.addEventListener('click', () => {
+//   navIsOpen = !navIsOpen;
+
+//   if (navIsOpen) {
+//     nav.className = 'hidden';
+//   } else {
+//     nav.className = '';
+//   }
+// });
