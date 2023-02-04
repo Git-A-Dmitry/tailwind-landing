4c835b2334fe0af2f601d45a74dbc49f6e1fa6a3
Dmitry A <adcntt@gmail.com>
2023-02-04 20:02:24 +0300

    some corrections

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const nav_list = document.querySelector('ul');
 const icon = document.querySelector('.menu-icon');
 
-// function toggleMenu(e) {
-//   e.name === 'menu' ? ((e.name = 'close'), nav_list.classList.add('top-[0px]'), nav_list.classList.add('opacity-100')) : ((e.name = 'menu'), nav_list.classList.remove('top-[0px]'), nav_list.classList.remove('opacity-100'));
-// }
+icon.onclick = function toggleMenu(e) {
+  nav_list.classList.add('top-[0px]');
+  // nav_list.classList.add('opacity-100');
+};
 
 nav_list.addEventListener('click', () => {
   nav_list.classList.remove('top-[0px]');
-  nav_list.classList.remove('opacity-100');
-  icon.name = 'menu';
+  // nav_list.classList.remove('opacity-100');
+  // icon.name = 'menu';
 });
