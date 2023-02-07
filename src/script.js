1fa85ba106dd98767b0ae557bbdb763512b88fab
Dmitry A <adcntt@gmail.com>
2023-02-07 16:05:28 +0300

    modal window

--- src/script.js
+++ src/script.js
@@ -1,9 +1,10 @@
-// Toggle Menu
 const nav_list = document.querySelector('ul');
 const icon = document.querySelector('.menu-icon');
+// let blur = document.getElementById('features');
+const btn_m = document.querySelector('.btn');
+const pop_up = document.querySelector('.popup');
 
-console.log(icon);
-
+// Toggle Menu
 icon.onclick = function toggleMenu() {
   nav_list.classList.add('top-[0px]');
   nav_list.classList.add('opacity-100');
@@ -14,6 +15,16 @@ nav_list.addEventListener('click', () => {
   nav_list.classList.remove('opacity-100');
   icon.name = 'menu';
 });
+
+// Pop-up window
+
+// blur.addEventListener('click', () => {
+//   blur.classList.toggle('active');
+// });
+
+// pop_btn.onclick = function toggleModal() {
+//   popup.classList.add('opacity-100');
+// };
 
 // Lazy loading
 const observer = new IntersectionObserver(
@@ -34,4 +45,18 @@ const observer = new IntersectionObserver(
 const hiddenElements = document.querySelectorAll('.lazy');
 hiddenElements.forEach((el) => observer.observe(el));
 
-console.log(hiddenElements);
+// btn_m.addEventListener('click', () => {
+// popup.classList.add('opacity-100');
+// popup.classList.add('scale-110');
+//   pop_up.target.classList.toggle('active');
+// });
+
+// console.log(btn_m);
+
+const toggle = () => {
+  // let blur = document.getElementById('blur');
+  // blur.classList.toggle('active');
+
+  // let popup = document.getElementById('popup');
+  pop_up.classList.toggle('active');
+};
